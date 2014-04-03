9b8895c65197b5e7c5af741e7632900c7bd8da3c
roller.coster <roller.coster@localhost.localdomain>
2014-04-03 13:13:11 +0530

    Major Deadline2 Commit1

---
+++ MajorAcc75/test/ShortTermCounter.java
@@ -0,0 +1,27 @@
+package parsing;
+
+public class ShortTermCounter {
+	
+	int type1;
+	int type2;
+	int type3;
+	int type4;
+	int type5;
+	int value;
+	ShortTermCounter()
+	{
+		type1=0;
+		type2=0;
+		type3=0;
+		type4=0;
+		type5=0;
+	}
+	
+	@Override
+	public String toString() {
+		
+		value = 50*type1 +30*type2 + 15*type3 + 5*type4 + type5;
+		return " |k 1:"+ value;
+	}
+	
+}
